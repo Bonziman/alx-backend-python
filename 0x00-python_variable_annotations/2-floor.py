f4289d29188a0adcc9f278f5cd23a6b34424ff39
Bonziman <eloirdiwi@gmail.com>
2024-10-11 21:46:11 +0100

    return floor number

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -7,12 +7,12 @@ This script returns the floor of a number
 
 def floor(n: float) -> float:
     """
-    this function returns the floot of a float number
+    this function returns the floor of a float number
 
     Args:
     n (float): the number to return it's float
 
     Returns:
-    the floor of n
+    float:the floor of n
     """
     return math.floor(n)
