8b8bae0f3d1ff7ae686a02ff64cb1e4594a9fc00
Bonziman <eloirdiwi@gmail.com>
2024-10-11 21:40:05 +0100

    return floor number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import math
+"""
+This script returns the floor of a number
+"""
+
+
+def floor(n: float) -> float:
+    """
+    this function returns the floot of a float number
+
+    Args:
+    n (float): the number to return it's float
+
+    Returns:
+    the floor of n
+    """
+    return math.floor(n)
