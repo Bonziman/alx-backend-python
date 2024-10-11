c5bbb382036cc68392bb8e8c50e9f73a15122d81
Bonziman <eloirdiwi@gmail.com>
2024-10-11 21:11:23 +0100

    first type annotatet function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+"""
+This script adds two numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers.
+
+    Args:
+    a (float): First number.
+    b (float): Second number.
+
+    Returns:
+    float: The result of adding a and b.
+    """
+    return a + b
