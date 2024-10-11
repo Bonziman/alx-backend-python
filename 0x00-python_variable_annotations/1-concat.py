53add100024b6a610b98304412549976ea723154
Bonziman <eloirdiwi@gmail.com>
2024-10-11 21:18:50 +0100

    concat annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This string concats two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concats two strings.
+
+    Args:
+  str1 (str): First string.
+  str2 (str): Second string.
+  Returns:
+  str: The oncatenated strings.
+  """
+    return "str1" + "str2"
