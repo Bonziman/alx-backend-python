9a7ab327c852d152be449a2e4325d7419a4746fc
Bonziman <eloirdiwi@gmail.com>
2024-10-11 21:13:27 +0100

    added hashbang to task

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,15 +3,17 @@
 This script adds two numbers.
 """
 
+
 def add(a: float, b: float) -> float:
+
     """
     Adds two numbers.
 
     Args:
     a (float): First number.
     b (float): Second number.
-
     Returns:
     float: The result of adding a and b.
     """
+
     return a + b
